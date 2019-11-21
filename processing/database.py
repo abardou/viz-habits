0fe0a2a1a3cb97fe39a092822293b65fe8d5a160
Anthony Bardou <a.bardou@outlook.fr>
2019-11-21 22:53:49 +0100

    User activity can be stored

--- processing/database.py
+++ processing/database.py
@@ -1,6 +1,16 @@
 import pymongo
 
-def get_database(db_name):
-    client = pymongo.MongoClient("mongodb+srv://thatuser:thatpassword@clusterthat-vxghd.mongodb.net/test?retryWrites=true&w=majority")
+# Cache for db object
+db_object = None
+
+def get_database():
+    if db_object is None:
+        client = pymongo.MongoClient("mongodb+srv://thatuser:thatpassword@clusterthat-vxghd.mongodb.net/test?retryWrites=true&w=majority")
+        return client.that
     
-    return client[db_name]
+    return db_object
+    
+# Give the activity collection
+def get_activity_collection():
+    db = get_database()
+    return db.Activity
