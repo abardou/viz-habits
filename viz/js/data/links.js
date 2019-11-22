d1c8422d83b47b778172ae5976ac164651b7e45e
Istoman <polloli.hugo@gmail.com>
2019-11-22 10:29:00 +0100

    delete useless code and prep for customization

--- viz/js/data/links.js
+++ viz/js/data/links.js
@@ -1,16 +1,8 @@
 export default [
-  { target: 'mammal', source: 'dog', strength: 0.7 },
-  { target: 'mammal', source: 'cat', strength: 0.7 },
-  { target: 'mammal', source: 'fox', strength: 0.7 },
-  { target: 'mammal', source: 'elk', strength: 0.7 },
-  { target: 'insect', source: 'ant', strength: 0.7 },
-  { target: 'insect', source: 'bee', strength: 0.7 },
-  { target: 'fish', source: 'carp', strength: 0.7 },
-  { target: 'fish', source: 'pike', strength: 0.7 },
-  { target: 'cat', source: 'elk', strength: 0.1 },
-  { target: 'carp', source: 'ant', strength: 0.1 },
-  { target: 'elk', source: 'bee', strength: 0.1 },
-  { target: 'dog', source: 'cat', strength: 0.1 },
-  { target: 'fox', source: 'ant', strength: 0.1 },
-  { target: 'pike', source: 'cat', strength: 0.1 }
-]
+  { target: 'screenoff', source: 'youtube', strength: 0.1 },
+  { target: 'screenonunlocked', source: 'screenoff', strength: 0.1 },
+  { target: 'appusage', source: 'screenonunlocked', strength: 0.2 },
+  { target: 'screenoff', source: 'appusage', strength: 0.1 },
+  { target: 'screenonlocked', source: 'screenoff', strength: 0.1 },
+  { target: 'screenonunlocked', source: 'screenonlocked', strength: 0.1 }
+];
