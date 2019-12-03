55261a52cab470326a6b093ddf90b3d3985d3344
Anthony Bardou <a.bardou@outlook.fr>
2019-12-03 18:21:31 +0100

    Better with absolute values in each attributes

--- viz/js/data/appchord_model.js
+++ viz/js/data/appchord_model.js
@@ -16,8 +16,6 @@ class AppChordModel {
         // Other attributes must NEVER change outside the constructor
         this.filtered_apps = this.apps.slice();
         this.filtered_apps_time = this.apps_time.slice();
-        this.filtered_cumul_time = this.cumul_time;
-        this.filtered_n_switches = this.n_switches.slice();
         this.filtered_adj_mat = this.adj_mat.slice();
     }
 
@@ -36,15 +34,10 @@ class AppChordModel {
         // Attribute apps_time : collection of proportion of time consumption for each app
         this.apps_time = new Array(this.apps.length).fill(0);
         // Attribute cumul_time : sum(apps_time)
-        this.cumul_time = 0;
         for (let d of data) {
             let app_idx = this.get_app_index(d['App Name'])
             this.apps_time[app_idx] += d['Duration'];
-            this.cumul_time += d['Duration'];
         }
-
-        for (let i in this.apps_time)
-            this.apps_time[i] = this.apps_time[i] / this.cumul_time
     }
 
     /**
@@ -63,7 +56,6 @@ class AppChordModel {
                            .map(d => {return new Array(this.apps.length).fill(0)})
 
         // Fill the matrix with each user sequence
-        this.n_switches = new Array(this.apps.length).fill(0);
         for (let uid of users) {
             // Build the user sequence
             let f_data = data.filter(d => d['User_ID'] == uid)
@@ -72,18 +64,11 @@ class AppChordModel {
             // Loop through the user sequence and compute switches
             for (let d of f_data) {
                 let app_idx = this.get_app_index(d['App Name']);
-                if (mem != undefined && mem != this.get_app_index(d['App Name'])) {
+                if (mem != undefined && mem != this.get_app_index(d['App Name']))
                     this.adj_mat[mem][app_idx] += 1;
-                    this.n_switches[mem] += 1;
-                }
                 mem = app_idx;
             }
         }
-
-        // Normalize the matrix
-        for (let line in this.adj_mat)
-            for (let col in this.adj_mat[line])
-                this.adj_mat[line][col] = this.adj_mat[line][col] / this.n_switches[line]
     }
 
     /**
@@ -104,45 +89,21 @@ class AppChordModel {
      * @param {Array(Array(float))} matrix the adjacency matrix of the graph
      * @param {Array(string)} app the collection of apps' names
      * @param {Array(int)} app_time the collection of proportions of app consumption time
-     * @param {int} c_time the cumulated consumption time
-     * @param {Array(int)} switches the number of times each app switched to another
-     * 
-     * @returns {int} the new cumulated consumption time
      */
-    remove_useless_apps(to_rem, matrix, app, app_time, c_time, switches) {
-        // Compute the absolute time from c_time and app_time
-        let abs_time = app_time.map(d => {return Math.round(c_time*d); })
+    remove_useless_apps(to_rem, matrix, app, app_time) {
         // Sort descending to_rem in order to prevent indexes issues
         to_rem = to_rem.sort(function(a, b) { return b - a; });
         // Remove apps filtered from attributes
         for (let i of to_rem) {
-            c_time -= abs_time[i]
             app.splice(i, 1)
             app_time.splice(i, 1)
 
-            switches.splice(i, 1)
             // Line of the matrix
             matrix.splice(i, 1)
             // Column of the matrix
             for (let j in matrix)
                 matrix[j].splice(i, 1)
         }
-
-        // Update n_switches
-        // Compute coeffs to apply to proportions into matrix at the same time
-        let coeffs = new Array(switches.length).fill(0)
-        for (let i in switches) {
-            let d = switches[i]
-            switches[i] = Math.round(d * matrix[i].reduce(function(a, b) {return a + b}))
-            if (switches[i] != 0)
-                coeffs[i] = d / switches[i];
-        }
-        // Update adj_mat values thanks to coeffs
-        for (let lig in matrix)
-            for (let col in matrix[lig])
-                matrix[lig][col] = coeffs[lig]*matrix[lig][col]
-
-        return c_time
     }
 
     /**
@@ -175,24 +136,20 @@ class AppChordModel {
      * @param {int} max_time the max criterion
      */
     apply_time_filter(min_time, max_time=Number.MAX_SAFE_INTEGER) {
-        // Build the absolute consumption time
-        let abs_time = this.apps_time.map(d => {return Math.round(this.cumul_time*d); })
         let to_rem = []
         // Apply the filter and collect apps indexes to remove
-        for (let i in abs_time)
-            if (abs_time[i] < min_time || abs_time[i] > max_time)
+        for (let i in this.filtered_apps_time)
+            if (this.filtered_apps_time[i] < min_time || this.filtered_apps_time[i] > max_time)
                 to_rem.push(i);
 
         // Filter with to_rem
         this.filtered_cumul_time = this.remove_useless_apps(to_rem, this.filtered_adj_mat, this.filtered_apps,
-                                                            this.filtered_apps_time, this.filtered_cumul_time,
-                                                            this.filtered_n_switches);
+                                                            this.filtered_apps_time);
 
         // The filter may have produced new useless apps, we remove them
         to_rem = this.get_useless_apps(this.filtered_adj_mat)
         this.filtered_cumul_time = this.remove_useless_apps(to_rem, this.filtered_adj_mat, this.filtered_apps,
-                                                            this.filtered_apps_time, this.filtered_cumul_time,
-                                                            this.filtered_n_switches);
+                                                            this.filtered_apps_time);
     }
 
     /**
@@ -202,18 +159,20 @@ class AppChordModel {
      * @param {float} max_freq the max criterion
      */
     apply_frequency_filter(min_freq, max_freq=1) {
+        // Compute sum of switches for each line of the matrix
+        let s_switches = this.filtered_adj_mat.map(d => d.reduce(function(a, b) { return a + b; }))
         // Replace each coefficient outside the filter by 0
-        this.filtered_adj_mat = this.filtered_adj_mat.map(d => {return d.map(c => {
-            if (c < min_freq || c > max_freq)
+        this.filtered_adj_mat = this.filtered_adj_mat.map(function(d, i) {return d.map(c => {
+            let coeff = c / s_switches[i]
+            if (coeff < min_freq || coeff > max_freq)
                 return 0
             return c
         })})
 
-        // This action may have produced useless apps, we remove them and update the matrix coefficients accordingly
+        // This action may have produced useless apps, we remove them
         let to_rem = this.get_useless_apps(this.filtered_adj_mat)
         this.filtered_cumul_time = this.remove_useless_apps(to_rem, this.filtered_adj_mat, this.filtered_apps,
-                                                            this.filtered_apps_time, this.filtered_cumul_time,
-                                                            this.filtered_n_switches);
+                                                            this.filtered_apps_time);
     }
 
     /**
@@ -228,8 +187,6 @@ class AppChordModel {
         // Copy the original data into the attributes to filter
         this.filtered_apps = this.apps.slice();
         this.filtered_apps_time = this.apps_time.slice();
-        this.filtered_cumul_time = this.cumul_time;
-        this.filtered_n_switches = this.n_switches.slice();
         this.filtered_adj_mat = this.adj_mat.slice();
 
         // Apply the filters
@@ -247,7 +204,7 @@ class AppChordModel {
 //             let f = d => d['App Name'] != "Screen off"
 //             data = data.filter(f)
 //             let acm = new AppChordModel(data)
-//             acm.apply_filters(0, Number.MAX_SAFE_INTEGER, 0.2, 0.5)
+//             acm.apply_filters(0, 5, 0, 1)
 //             console.log(acm.filtered_adj_mat)
 //         }
 // };
