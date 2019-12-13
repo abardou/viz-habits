35a7162b9c36f38832f817305937d4c2ca40f421
Anthony Bardou <a.bardou@outlook.fr>
2019-12-13 15:33:04 +0100

    D3 RangeSlider under dev #5

--- viz-site/src/components/filters/RangeSlider.vue
+++ viz-site/src/components/filters/RangeSlider.vue
@@ -1,18 +1,5 @@
 <template>
-	<v-container>
-		<v-row>
-			<v-container id="slider-container" />
-			<v-col cols="3" sm="12" md="3">
-				<v-range-slider
-					v-model="selected"
-					thumb-label="always"
-					:min="min"
-					:max="max"
-					:step="step"
-				/>
-			</v-col>
-		</v-row>
-	</v-container>
+	<svg id="slider" />
 </template>
  
 <script>
@@ -29,8 +16,12 @@ export default {
 			default: null,
 			type: Number
 		},
-		height: {
+		heightHist: {
 			default: 175,
+			type: Number
+		},
+		heightSlid: {
+			default: 80,
 			type: Number
 		},
 		width: {
@@ -40,7 +31,13 @@ export default {
 	},
 	data: () => ({
 		svg: null,
-		selected: [5, 10]
+		bins: null,
+		thickness: 4,
+		cursorRad: 7,
+		selColor: '#69b3a2',
+		unselColor: '#ddd',
+		margin: {top: 10, left: 10, right:10, bottom:10, bet: 20},
+		selected: [0, 1]
 	}),
 	computed: {
 		min: function() {
@@ -51,54 +48,68 @@ export default {
 		},
 		range: function() {
 			return [this.min + 0.2*(this.max - this.min), this.min + 0.8*(this.max - this.min)];
-		},
-		bins: function() {
-			let nb_bins = (this.max - this.min) / this.step;
-			let b = new Array(nb_bins).fill(0);
-
-			// Fill the bins
-			for (let d of this.data) {
-				let ibin = Math.floor((d - this.min) / this.step);
-				b[ibin] += 1;
-			}
-
-			return b;
 		}
 	},
 	mounted() {
 		let that = this;
 
-		this.svg = d3.select('#slider-container').append('svg')
+		this.svg = d3.select('#slider')
 			.attr('width', this.width)
-			.attr('height', this.height);
+			.attr('height', this.heightHist + this.heightSlid + this.margin.bet + this.thickness)
+			.attr('transform', 'translate('+this.margin.left+','+this.margin.top+')');
 
 		let nbBins = (this.max - this.min) / this.step;
 
+		// Draw histogram
 		var x = d3.scaleLinear()
 			.domain([this.min, this.max])
 			.range([0, this.width]);
 
 		var y = d3.scaleLinear()
-			.range([this.height, 0]);
+			.range([this.heightHist, 0]);
 
 		var histogram = d3.histogram()
 			.domain(x.domain())
 			.thresholds(x.ticks(nbBins));
-		var bins = histogram(this.data);
+		this.bins = histogram(this.data);
+		this.selected = [this.bins[parseInt(0.25*this.bins.length)].x0, this.bins[parseInt(0.75*this.bins.length)].x1];
 
-		y.domain([0, d3.max(bins, function(d) { return d.length; })]);
+		y.domain([0, d3.max(this.bins, function(d) { return d.length; })]);
 
 		var u = this.svg.selectAll('rect')
-			.data(bins);
+			.data(this.bins);
 
 		u.enter()
 			.append('rect') // Add a new rect for each new elements
 			.attr('x', d => x(d.x0))
 			.attr('y', d => y(d.length))
 			.attr('width', function(d) { return x(d.x1) - x(d.x0) - 1; })
-			.attr('height', function(d) { return that.height - y(d.length); })
-			.style('fill', '#69b3a2');
+			.attr('height', function(d) { return that.heightHist - y(d.length); })
+			.style('fill', d => d.x0 >= this.selected[0] && d.x1 <= this.selected[1] ? this.selColor : this.unselColor);
 		
+		// Draw slider
+		let l_cor = [
+			{x0: 0, x1: this.selected[0], c:this.unselColor},
+			{x0: this.selected[0], x1: this.selected[1], c:this.selColor},
+			{x0: this.selected[1], x1: this.max, c:this.unselColor}
+		];
+		this.svg.selectAll('slines').data(l_cor)
+			.enter()
+			.append('line')
+			.attr('x1', d => x(d.x0))
+			.attr('y1', this.heightHist + this.margin.bet)
+			.attr('x2', d => x(d.x1))
+			.attr('y2', this.heightHist + this.margin.bet)
+			.attr('stroke', d => d.c)
+			.attr('stroke-width', this.thickness);
+			
+		this.svg.selectAll('cursors').data(this.selected)
+			.enter()
+			.append('circle')
+			.attr('cx', d => x(d))
+			.attr('cy', d => this.heightHist + this.margin.bet)
+			.attr('r', this.cursorRad)
+			.style('fill', this.selColor);
 	}
 };
 </script>
