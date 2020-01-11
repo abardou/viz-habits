62fa005e0c3db8d265189745a8fbc73763fdd387
Unknown <polloli.hugo@gmail.com>
2020-01-11 18:29:02 +0100

    really enable scroll this time

--- viz-site/src/components/visus/RadialTree.vue
+++ viz-site/src/components/visus/RadialTree.vue
@@ -475,8 +475,6 @@ export default {
 </script>
 
 <style>
-	body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; height: 3000px }
-	/*
 	.hidden {
 		display: none;
 	}
@@ -510,5 +508,4 @@ export default {
 		font-family: sans-serif;
 		font-size: 0.5em;
 	}
-	*/
 </style>
